1125d483184a66bf8e8dfe01cb7afe69293c8b54
qtrandeveloper <qtrandeveloper@outlook.com>
2017-07-20 22:37:38 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+//accept character number input
+//assign number to password string length
+//onlick generate button creates random string of number length
+//password will be made up of characters a-z, A-Z, 0-9
+//how to randomly create string?
+//return password to input field after generated
+//copy password button
