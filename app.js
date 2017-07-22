6baf2610bd691f9e063b48c4fa4c5e1e6c5c5e70
qtrandeveloper <qtrandeveloper@outlook.com>
2017-07-22 23:45:09 +0800

    why won't this work?

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
-//accept character number input
-//assign number to password string length
-//onlick generate button creates random string of number length
-//password will be made up of characters a-z, A-Z, 0-9
-//how to randomly create string?
-//assign string to password
-//return password to input field after generated
-
 function generatePassword() {
-var passwordLength = document.getElementById("chars-number").value;
-var characters = "";
+var passwordLength = document.input.charsnumber.value;
+var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 var newPassword = "";
-
-document.getElementById("new-password").value = newPassword;
+        for (var i = 0; i < passwordLength; i++) {
+             var rnum = Math.floor(Math.random() * chars.length);
+		     newPassword += chars.substring(rnum,rnum+1);
+            }        
+document.output.newpassword.value = newPassword;
 }
 
 
-// how to copy input from form and copy to clipboard?
+
 function copyPassword() {
-var toBeCopied = document.getElementById("new-password").value;
+var toBeCopied = document.getElementById("newpassword").value;
 }
