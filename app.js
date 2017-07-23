db917bc413530662921a27f266a6ff94527d24c4
qtrandeveloper <qtrandeveloper@outlook.com>
2017-07-23 21:03:00 +0800

    better looks

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ var newPassword = "";
         for (var i = 0; i < passwordLength; i++) {
              var rnum = Math.floor(Math.random() * chars.length);
 		     newPassword += chars.substring(rnum,rnum+1);
-            }        
+            }
 document.getElementById("newpassword").value = newPassword;
 }
 
+
+
 function copyPassword() {
-var toBeCopied = document.getElementById("newpassword").value;
+var toBeCopied = document.getElementById("newpassword").value; //sub newPassword
 
 
 }
