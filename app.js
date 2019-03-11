7f260d0f9681f2ada3af6bc86af2b2e697b7f954
Que Tran <quet928@gmail.com>
2019-03-11 09:04:48 +0800

    removed inline js, reworking password functions

--- app.js
+++ app.js
@@ -1,41 +1,30 @@
-var button = document.getElementById("copy-button");
+document.getElementById("gen-pass").addEventListener("click", generatePassword);
 
+// WHY ISNT MY CODE WORKING?
 function generatePassword() {
-      var passwordLength = document.getElementById("number").value;
+      var passwordLength = document.getElementById("chars-number").value;
       var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-		if (passwordLength < 5) {
-		document.getElementById("number").value = "Your password needs to be longer";
+
+		if (passwordLength < 10) {
+		document.getElementById("chars-number").value = "Your password needs to be longer";
 	} 
+  
 	else {
 		      var newPassword = "";
               for (var i = 0; i < passwordLength; i++) {
                    var rnum = Math.floor(Math.random() * chars.length);
-      		     newPassword += chars.substring(rnum,rnum+1);
+                   newPassword += chars.substring(rnum,rnum+1);
                   }
-	}
-      document.getElementById("new-password").value = newPassword;
+        } 
+    document.getElementById("new-password").value = newPassword;
 }
 
 
-button.addEventListener("click", function() {
-
-    // We will need a range object and a selection.
-    var range = document.createRange(),
-        selection = window.getSelection();
-
-    // Clear selection from any previous data.
-    selection.removeAllRanges();
-
-    // Make the range select the entire content of password
-    range.selectNodeContents();
-
-    // Add that range to the selection.
-    selection.addRange(range);
-
-    // Copy the selection to clipboard.
+document.getElementById("copy-button").addEventListener("click", function() {
+	
+	var copiedText = document.getElementById("new-password");
+    copiedText.select();
     document.execCommand('copy');
-
-    // Clear selection if you want to.
-    selection.removeAllRanges();
-
-}, true);
+	alert("New password copied to clipboard!");
+	
+});
